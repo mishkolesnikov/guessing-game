b0d7dafc9eada55b8cbab842e51c26807fb81fe4
mishkolesnikov <mishkolesnikov@gmail.com>
2017-02-05 18:17:47 +0300

    Implement binary search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this._minValue = min;
+        this._maxValue = max;
     }
 
     guess() {
-
+        this._assumption = this._maxValue - this._minValue;
+        this._assumption = this._assumption/2;
+        this._assumption = Math.floor(this._assumption);
+        this._assumption = this._maxValue - this._assumption;
+        return this._assumption;
     }
 
     lower() {
-
+        this._maxValue = this._assumption;
     }
 
     greater() {
-
+        this._minValue = this._assumption;
     }
 }
 
